8ea45b3d2110ab15e6a98c357df0afe6a4ec1077
Yousef Javaherian <yousefjavaherian@gmail.com>
2022-11-30 23:14:39 +0330

    server is ready for playground when it updates to trpc10

--- server/index.ts
+++ server/index.ts
@@ -1,24 +1,38 @@
 import express from "express"
 import * as trpcExpress from "@trpc/server/adapters/express"
+// import { expressHandler } from "trpc-playground/handlers/express"
 import { createContext } from "./context"
 import { appRouter } from "./routers/_app"
+
 const PORT = 2022
 
-const app = express()
-app.get("/", (_req, res) => res.send("Express + Prisma + tRPC 👋"))
+const main = async () => {
+	const app = express()
+	app.get("/", (_req, res) => res.send("Express + Prisma + tRPC 👋"))
 
-app.use((req, _res, next) => {
-	// request logger
-	console.log("⬅️ ", req.method, req.path, req.body ?? req.query)
-	next()
-})
-app.use(
-	"/trpc",
-	trpcExpress.createExpressMiddleware({
-		router: appRouter,
-		createContext,
+	app.use((req, _res, next) => {
+		// request logger
+		console.log("⬅️ ", req.method, req.path, req.body ?? req.query)
+		next()
 	})
-)
-app.listen(PORT, () =>
-	console.log(`server listening at http://localhost:${PORT} 🚀`)
-)
+	app.use(
+		"/trpc",
+		trpcExpress.createExpressMiddleware({
+			router: appRouter,
+			createContext,
+		})
+	)
+	// app.use(
+	// 	"/play",
+	// 	await expressHandler({
+	// 		trpcApiEndpoint: "/trpc",
+	// 		playgroundEndpoint: "/play",
+	// 		router: appRouter,
+	// 	})
+	// )
+
+	app.listen(PORT, () =>
+		console.log(`server listening at http://localhost:${PORT} 🚀`)
+	)
+}
+main()
