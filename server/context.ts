9f5dbe7869aa8ee9e18e8ca4102ba550bbcb85d6
Yousef Javaherian <yousefjavaherian@gmail.com>
2022-12-02 00:18:16 +0330

    adding JWT auth and RBAC middleware

--- server/context.ts
+++ server/context.ts
@@ -1,14 +1,28 @@
-import { PrismaClient } from "@prisma/client"
-import { inferAsyncReturnType } from "@trpc/server"
+import { inferAsyncReturnType, TRPCError } from "@trpc/server"
 import * as trpcExpress from "@trpc/server/adapters/express"
-
-export const createContext = ({
+import jwt from "jsonwebtoken"
+import { prisma } from "./globals"
+export const createContext = async ({
 	req,
 	res,
 }: trpcExpress.CreateExpressContextOptions) => {
-	return {
-		req,
-		res,
+	try {
+		const { authorization } = req.headers
+		const token = authorization?.split(" ").at(1) // authorization = bearer token
+		if (token === undefined) return { user: null }
+		const { id } = jwt.verify(token, process.env.JWT_SECRET) as { id: string }
+		const user = await prisma.user.findFirst({ where: { id } })
+		if (user === null)
+			throw new TRPCError({
+				message: "account no longer exists, please login again",
+				code: "BAD_REQUEST",
+			})
+		return { user }
+	} catch (error) {
+		throw new TRPCError({
+			message: "token expired please login again",
+			code: "BAD_REQUEST",
+		})
 	}
 }
 
